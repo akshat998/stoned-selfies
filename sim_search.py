44c32dce495c91007d6e1c0949b33eeeb2a74f67
akshat998 <akshat998@gmail.com>
2020-12-19 15:17:55 -0500

    Experiment B added

--- sim_search.py
+++ sim_search.py
@@ -1,9 +1,7 @@
 import time 
 import selfies
 import rdkit
-import random
 import numpy as np
-import random
 from rdkit import Chem
 from selfies import encoder, decoder
 from rdkit.Chem import MolFromSmiles as smi2mol
@@ -18,15 +16,18 @@ from rdkit import RDLogger
 RDLogger.DisableLog('rdApp.*')
 
 def randomize_smiles(mol):
-    """
-    Returns a random (dearomatized) SMILES given an rdkit mol object of a molecule.
+    """Returns a random (dearomatized) SMILES given an rdkit mol object of a molecule.
 
+    Parameters:
+    mol (rdkit.Chem.rdchem.Mol) :  RdKit mol object (None if invalid smile string smi)
+    
+    Returns:
+    mol (rdkit.Chem.rdchem.Mol) : RdKit mol object  (None if invalid smile string smi)
     """
     if not mol:
         return None
 
     Chem.Kekulize(mol)
-    
     return rdkit.Chem.MolToSmiles(mol, canonical=False, doRandom=True, isomericSmiles=False,  kekuleSmiles=True)
 
 
@@ -71,12 +72,15 @@ def get_selfie_chars(selfie):
 
 
 class _FingerprintCalculator:
-    """
-    Calculate the fingerprint while avoiding a series of if-else.
-    See recipe 8.21 of the book "Python Cookbook".
+    ''' Calculate the fingerprint for a molecule, given the fingerprint type
 
-    To support a new type of fingerprint, just add a function "get_fpname(self, mol)".
-    """
+    Parameters: 
+        mol (rdkit.Chem.rdchem.Mol) : RdKit mol object (None if invalid smile string smi)
+        fp_type (string)            :Fingerprint type  (choices: AP/PHCO/BPF,BTF,PAT,ECFP4,ECFP6,FCFP4,FCFP6)  
+
+    Returns:
+        RDKit fingerprint object
+    '''
 
     def get_fingerprint(self, mol: Mol, fp_type: str):
         method_name = 'get_' + fp_type
@@ -114,15 +118,27 @@ class _FingerprintCalculator:
 
 
 def get_fingerprint(mol: Mol, fp_type: str):
+    ''' Fingerprint getter method. Fingerprint is returned after using object of 
+        class '_FingerprintCalculator'
+        
+    Parameters: 
+        mol (rdkit.Chem.rdchem.Mol) : RdKit mol object (None if invalid smile string smi)
+        fp_type (string)            :Fingerprint type  (choices: AP/PHCO/BPF,BTF,PAT,ECFP4,ECFP6,FCFP4,FCFP6)  
+
+    Returns:
+        RDKit fingerprint object
+        
+    '''
     return _FingerprintCalculator().get_fingerprint(mol=mol, fp_type=fp_type)
 
 def mutate_selfie(selfie, max_molecules_len, write_fail_cases=False):
-    '''Return a mutated selfie string
+    '''Return a mutated selfie string (only one mutation on slefie is performed)
     
     Mutations are done until a valid molecule is obtained 
-    Rules of mutation: With a 50% propbabily, either: 
+    Rules of mutation: With a 33.3% propbabily, either: 
         1. Add a random SELFIE character in the string
         2. Replace a random SELFIE character with another
+        3. Delete a random character
     
     Parameters:
     selfie            (string)  : SELFIE string to be mutated 
@@ -194,6 +210,16 @@ def mutate_selfie(selfie, max_molecules_len, write_fail_cases=False):
     return (selfie_mutated, smiles_canon)
 
 def get_mutated_SELFIES(selfies_ls, num_mutations): 
+    ''' Mutate all the SELFIES in 'selfies_ls' 'num_mutations' number of times. 
+    
+    Parameters:
+    selfies_ls   (list)  : A list of SELFIES 
+    num_mutations (int)  : number of mutations to perform on each SELFIES within 'selfies_ls'
+    
+    Returns:
+    selfies_ls   (list)  : A list of mutated SELFIES
+    
+    '''
     for _ in range(num_mutations): 
         selfie_ls_mut_ls = []
         for str_ in selfies_ls: 
@@ -211,16 +237,8 @@ def get_mutated_SELFIES(selfies_ls, num_mutations):
 def get_fp_scores(smiles_back, target_smi, fp_type): 
     smiles_back_scores = []
     target    = Chem.MolFromSmiles(target_smi)
-    # fp_target = get_ECFP4(target)
+
     fp_target = get_fingerprint(target, fp_type)
-    
-    # init_  = Chem.MolFromSmiles('CCCCC')
-    # target = Chem.MolFromSmiles('CCCCC')
-    # init_fp = get_fingerprint(init_, 'AP')
-    # target_fp = get_fingerprint(target, 'AP')
-    
-    # from rdkit.DataStructs.cDataStructs import TanimotoSimilarity
-    # score  = TanimotoSimilarity(init_fp, target_fp)
 
     for item in smiles_back: 
         mol    = Chem.MolFromSmiles(item)
@@ -230,121 +248,89 @@ def get_fp_scores(smiles_back, target_smi, fp_type):
         smiles_back_scores.append(score)
     return smiles_back_scores
     
-total_time = time.time()
-num_random_samples = 50000 
-
-repr_type          = 1 # 1=SELFIES; 2=Smiles; 3=DeepSmiles
-num_mutation_ls    = [1, 2, 3, 4, 5]
-# fp_type            = 'AP'
-# fp_type            = 'FCFP4'
-fp_type            = 'ECFP4'
-
-# smi = 'C1CC(=O)NC2=C1C=CC(=C2)OCCCCN3CCN(CC3)C4=C(C(=CC=C4)Cl)Cl'   # Aripiprazole
-# smi = 'CC(C)(C)NCC(C1=CC(=C(C=C1)O)CO)O'                            # Albuterol
-# smi = 'CC12CCC3C(C1CCC2(C#C)O)CCC4=C3C=CC(=C4)OC'                   # Mestranol
-smi = 'CC1=CC=C(C=C1)C2=CC(=NN2C3=CC=C(C=C3)S(=O)(=O)N)C(F)(F)F'    # Celecoxib
-# smi = 'C1CC1N2C=C(C(=O)C3=CC(=C(C=C32)N4CCNCC4)F)C(=O)O'              # Ciprofloxacin
-# smi = 'C'
-
-mol = Chem.MolFromSmiles(smi)
-if mol == None: 
-    raise Exception('Invalid starting structure encountered')
+if __name__ == "__main__": 
     
-start_time = time.time()
-randomized_smile_orderings  = [randomize_smiles(mol) for _ in range(num_random_samples)]
+    # Executable code for EXPERIMENT B: 
+        
+    # Aripirazole starting structure: 
+    smi     = 'C1CC(=O)NC2=C1C=CC(=C2)OCCCCN3CCN(CC3)C4=C(C(=CC=C4)Cl)Cl'   # Aripiprazole
+    fp_type = 'ECFP4'
+        
+    # Albuterol starting structure: 
+    # smi     = 'CC(C)(C)NCC(C1=CC(=C(C=C1)O)CO)O'                            # Albuterol
+    # fp_type = 'FCFP4'
 
-# Convert all the molecules to SELFIES
-selfies_ls = [encoder(x) for x in randomized_smile_orderings]
-print('Randomized molecules (in SELFIES) time: ', time.time()-start_time)
+        
 
+    # Mestranol starting structure: 
+    # smi     = 'CC12CCC3C(C1CCC2(C#C)O)CCC4=C3C=CC(=C4)OC'                   # Mestranol
+    # fp_type = 'AP'
 
-all_smiles_collect = []
-all_smiles_collect_broken = []
-
-start_time = time.time()
-for num_mutations in num_mutation_ls: 
-    # Mutate the SELFIE string: 
-    selfies_mut = get_mutated_SELFIES(selfies_ls.copy(), num_mutations=num_mutations)
+        
+    total_time = time.time()
+    # num_random_samples = 50000 # TODO 
+    num_random_samples = 1000     
+    num_mutation_ls    = [1, 2, 3, 4, 5]
     
-    # Convert back to SMILES: 
-    smiles_back = [decoder(x) for x in selfies_mut]
-    all_smiles_collect = all_smiles_collect + smiles_back
-    all_smiles_collect_broken.append(smiles_back)
+    mol = Chem.MolFromSmiles(smi)
+    if mol == None: 
+        raise Exception('Invalid starting structure encountered')
+        
+    start_time = time.time()
+    randomized_smile_orderings  = [randomize_smiles(mol) for _ in range(num_random_samples)]
     
-    # Get fingerprint scores for all of them: 
-    # smiles_back_scores = get_fp_scores(smiles_back, target_smi=smi, fp_type=fp_type)
+    # Convert all the molecules to SELFIES
+    selfies_ls = [encoder(x) for x in randomized_smile_orderings]
+    print('Randomized molecules (in SELFIES) time: ', time.time()-start_time)
     
-    # print('Num mutations: ', num_mutations)
-    # print('    # New smile orderings = ', len(randomized_smile_orderings))
-    # print('    Delta > 0.8: ', (len([x for x in smiles_back_scores if x > 0.8]) / len(randomized_smile_orderings)) * 100)
-    # print('    Delta > 0.6: ', (len([x for x in smiles_back_scores if x > 0.6]) / len(randomized_smile_orderings)) * 100)
-    # print('    Delta > 0.4: ', (len([x for x in smiles_back_scores if x > 0.4]) / len(randomized_smile_orderings)) * 100) 
-print('Mutation obtainment time (back to smiles): ', time.time()-start_time)
+    
+    all_smiles_collect = []
+    all_smiles_collect_broken = []
+    
+    start_time = time.time()
+    for num_mutations in num_mutation_ls: 
+        # Mutate the SELFIES: 
+        selfies_mut = get_mutated_SELFIES(selfies_ls.copy(), num_mutations=num_mutations)
+        
+        # Convert back to SMILES: 
+        smiles_back = [decoder(x) for x in selfies_mut]
+        all_smiles_collect = all_smiles_collect + smiles_back
+        all_smiles_collect_broken.append(smiles_back)
+        
 
+    print('Mutation obtainment time (back to smiles): ', time.time()-start_time)
+    
+    
+    # Work on:  all_smiles_collect
+    start_time = time.time()
+    canon_smi_ls = []
+    for item in all_smiles_collect: 
+        mol, smi_canon, did_convert = sanitize_smiles(item)
+        if mol == None or smi_canon == '' or did_convert == False: 
+            raise Exception('Invalid smile string found')
+        canon_smi_ls.append(smi_canon)
+    canon_smi_ls        = list(set(canon_smi_ls))
+    print('Unique mutated structure obtainment time: ', time.time()-start_time)
+    
+    start_time = time.time()
+    canon_smi_ls_scores = get_fp_scores(canon_smi_ls, target_smi=smi, fp_type=fp_type)
+    print('Fingerprint calculation time: ', time.time()-start_time)
+    print('Total time: ', time.time()-total_time)
+    print()
+    
 
-# Work on:  all_smiles_collect
-start_time = time.time()
-canon_smi_ls = []
-for item in all_smiles_collect: 
-    mol, smi_canon, did_convert = sanitize_smiles(item)
-    if mol == None or smi_canon == '' or did_convert == False: 
-        raise Exception('Invalid smile string found')
-    canon_smi_ls.append(smi_canon)
-canon_smi_ls        = list(set(canon_smi_ls))
-print('Unique mutated structure obtainment time: ', time.time()-start_time)
-
-start_time = time.time()
-canon_smi_ls_scores = get_fp_scores(canon_smi_ls, target_smi=smi, fp_type=fp_type)
-print('Fingerprint calculation time: ', time.time()-start_time)
-print('Total time: ', time.time()-total_time)
-
-# THE TOP 100 MOLECULES ################################
-a          = np.argsort(canon_smi_ls_scores)[::-1][:100]
-scores_top = [canon_smi_ls_scores[i] for i in a ]
-smi_top    = [canon_smi_ls[i] for i in a ]
-
-thrh_score = []
-for item in scores_top: 
-    if item >= 0.75: 
-        thrh_score.append(1.0)
-    else: 
-        thrh_score.append((4/3)*item)
-# raise Exception('TESTING :) ')
-score_1 = thrh_score[0]
-score_10 = np.sum(thrh_score[0:10]) / 10
-score_100 = np.sum(thrh_score[0:100]) / 100
-print()
-print('Total score: ', (score_1+score_10+score_100)/3)
-# THE TOP 100 MOLECULES ################################
-
-# print('    Delta > 0.8: ', len([x for x in canon_smi_ls_scores if x > 0.8]) )
-A, B, C = [x for x in canon_smi_ls_scores if x > 0.75], [x for x in canon_smi_ls_scores if x > 0.6], [x for x in canon_smi_ls_scores if x > 0.4]
-
-print('    Delta > 0.75: ', len(A) )
-print('    Delta > 0.6: ', len(B))
-print('    Delta > 0.4: ', len(C))
-
-indices_thresh_8 = [i for i,x in enumerate(canon_smi_ls_scores) if x > 0.8]
-mols_8 = [canon_smi_ls[idx] for idx in indices_thresh_8]
-
-
-indices_thresh_6 = [i for i,x in enumerate(canon_smi_ls_scores) if x > 0.6 and x < 0.8]
-mols_6 = [canon_smi_ls[idx] for idx in indices_thresh_6]
-
-indices_thresh_4 = [i for i,x in enumerate(canon_smi_ls_scores) if x > 0.4 and x < 0.6]
-mols_4 = [canon_smi_ls[idx] for idx in indices_thresh_4]
-
-print('    Delta > 0.75: ', (len(A)/len(canon_smi_ls_scores))*100 )
-print('    Delta > 0.6: ', (len(B)/len(canon_smi_ls_scores))*100)
-print('    Delta > 0.4: ', (len(C)/len(canon_smi_ls_scores))*100)
-
-
-# Period -> newline
-# Header-> smiles
-
-with open('./right.txt', 'w') as f: 
-    f.writelines(['smiles\n', '\n'.join(x for x in mols_4)])
-
+    A, B, C = [x for x in canon_smi_ls_scores if x > 0.75], [x for x in canon_smi_ls_scores if x > 0.6], [x for x in canon_smi_ls_scores if x > 0.4]
+    
+    print('Number of molecules: ')
+    print('    # Delta > 0.75: ', len(A) )
+    print('    # Delta > 0.6: ', len(B))
+    print('    # Delta > 0.4: ', len(C))
+    print()
+    print('Percentage of total molecules: ')
+    print('    % Delta > 0.75: ', (len(A)/len(canon_smi_ls_scores))*100 )
+    print('    % Delta > 0.6: ', (len(B)/len(canon_smi_ls_scores))*100)
+    print('    % Delta > 0.4: ', (len(C)/len(canon_smi_ls_scores))*100)
+    
 
 
 
